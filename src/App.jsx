d1a83560df7eb4c2fc6e71c6c614cc7d31039e73
John Pennyman <johnpennyman95@gmail.com>
2024-07-25 11:24:46 -0500

    create edit Job page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import JobsPage from "./pages/JobsPage";
 import AddJobPage from "./pages/AddJobPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, { jobLoader } from "./pages/JobPage";
+import EditJobPage from "./pages/EdidJobPage";
 
 const addJob = async (newJob) => {
     const res = await fetch("/api/jobs", {
@@ -18,6 +19,22 @@ const addJob = async (newJob) => {
             "Content-Type": "applicaiton/json",
         },
         body: JSON.stringify(newJob),
+    });
+    return;
+};
+const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+        method: "DELETE",
+    });
+    return;
+};
+const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(job),
     });
     return;
 };
@@ -33,8 +50,13 @@ const App = () => {
                     element={<AddJobPage addJobSubmit={addJob} />}
                 />
                 <Route
+                    path="/edit-job/:id"
+                    element={<EditJobPage updateJobSubmit={updateJob} />}
+                    loader={jobLoader}
+                />
+                <Route
                     path="/jobs/:id"
-                    element={<JobPage />}
+                    element={<JobPage deleteJob={deleteJob} />}
                     loader={jobLoader}
                 />
                 <Route path="*" element={<NotFoundPage />} />
